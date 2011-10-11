0ff504dff19411b85695b42176354b8332a89de0
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-11 16:15:00 -0300

    Update project Add NLog.config Add XBeeUnmarshalerTest

---
+++ XBee.Test/src/XBee/XBeeUnmarshalerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using XBee;
+using XBee.Exceptions;
+
+namespace XBee.Test
+{
+    [TestFixture]
+    class XBeeUnmarshalerTest
+    {
+
+        public class NotXBeeFrame
+        {
+            public NotXBeeFrame() { }
+        }
+
+        public class XBeeUnknownFrame : XBeeFrame
+        {
+            public XBeeUnknownFrame() { }
+        }
+
+        [Test]
+        [ExpectedException(typeof(XBeeException), ExpectedMessage = "Invalid Frame Handler")]
+        public void TestXBeeUnmarshalerRegisterWrong()
+        {
+            XBeePacketUnmarshaler.registerResponseHandler(XBeeAPICommandId.REMOTE_AT_REQUEST, typeof(NotXBeeFrame));
+        }
+
+        [Test]
+        public void TestXBeeUnmarshalerRegister()
+        {       
+            XBeePacketUnmarshaler.registerResponseHandler(XBeeAPICommandId.UNKNOWN, typeof(XBeeUnknownFrame));
+        }
+
+    }
+}
