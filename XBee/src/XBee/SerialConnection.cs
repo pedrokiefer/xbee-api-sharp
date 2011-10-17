79c86c6de7e94129f9e1edba65162718ce933e1d
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-17 15:31:27 -0200

    Implement missing methods

--- XBee/src/XBee/SerialConnection.cs
+++ XBee/src/XBee/SerialConnection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.IO.Ports;
 
 namespace XBee
@@ -9,5 +10,20 @@ namespace XBee
     public class SerialConnection : XBeeConnection
     {
         private SerialPort serialPort;
+
+        public SerialConnection()
+        {
+            serialPort = new SerialPort();
+        }
+
+        public Stream GetStream()
+        {
+            return serialPort.BaseStream;
+        }
+
+        public void Close()
+        {
+
+        }
     }
 }
