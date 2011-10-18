62bd1d575ca6527b668e7bb411df92e7f6299a7d
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-17 23:42:21 -0200

    Move frame tests to a subdirectory

--- XBee.Test/src/XBee/Frames/ATCommandTest.cs
+++ XBee.Test/src/XBee/Frames/ATCommandTest.cs
@@ -6,17 +6,24 @@ using NUnit.Framework;
 using XBee;
 using XBee.Frames;
 
-namespace XBee.Test
+namespace XBee.Test.Frames
 {
     [TestFixture]
     class ATCommandTest
     {
         [Test]
+        public void TestATCommandND()
+        {
+            ATCommand cmd = new ATCommand(AT.ND);
+            Assert.AreEqual(new byte[] { 0x08, 0x00, (byte)'N', (byte)'D' }, cmd.ToByteArray());
+        }
+
+        [Test]
         public void TestATCommandDH()
         {
             ATCommand cmd = new ATCommand(AT.DH);
             cmd.SetValue(0x11223300);
-            Assert.AreEqual(new byte[] { 0x08, 0x00, (byte) 'D', (byte) 'H', 0x11, 0x22 }, cmd.ToByteArray());
+            Assert.AreEqual(new byte[] { 0x08, 0x00, (byte)'D', (byte)'H', 0x11, 0x22, 0x33, 0x00 }, cmd.ToByteArray());
         }
 
         [Test]
@@ -24,7 +31,7 @@ namespace XBee.Test
         {
             ATCommand cmd = new ATCommand(AT.DH);
             cmd.FrameId = 0x02;
-            Assert.AreEqual(new byte[] { 0x08, 0x02, (byte) 'D', (byte) 'H'}, cmd.ToByteArray());
+            Assert.AreEqual(new byte[] { 0x08, 0x02, (byte)'D', (byte)'H' }, cmd.ToByteArray());
         }
 
         [Test]
@@ -32,7 +39,7 @@ namespace XBee.Test
         {
             ATCommand cmd = new ATQueueCommand(AT.DH);
             cmd.SetValue(0x11223300);
-            Assert.AreEqual(new byte[] { 0x09, 0x00, (byte) 'D', (byte) 'H', 0x11, 0x22 }, cmd.ToByteArray());
+            Assert.AreEqual(new byte[] { 0x09, 0x00, (byte)'D', (byte)'H', 0x11, 0x22, 0x33, 0x00 }, cmd.ToByteArray());
         }
     }
 }
