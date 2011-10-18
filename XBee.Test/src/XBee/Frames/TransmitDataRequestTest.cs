2a78c8ac5cf85d9eb1ffee8969195acfeaada213
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-17 23:43:53 -0200

    Implements Transmit Data Request Frame

---
+++ XBee.Test/src/XBee/Frames/TransmitDataRequestTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using XBee;
+using XBee.Frames;
+
+namespace XBee.Test.Frames
+{
+    [TestFixture]
+    class TransmitDataRequestTest
+    {
+        [Test]
+        public void TestTransmitDataRequestBroadcast()
+        {
+            XBeeNode broadcast = new XBeeNode();
+            broadcast.Address16 = XBeeAddress16.ZNET_BROADCAST;
+            broadcast.Address64 = XBeeAddress64.BROADCAST;
+
+            TransmitDataRequest frame = new TransmitDataRequest(broadcast);
+            Assert.AreEqual(new byte[] { 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFE, 0x00, 0x00 }, frame.ToByteArray());
+        }
+
+        [Test]
+        public void TestTransmitDataRequestBroadcastFrameId()
+        {
+            XBeeNode broadcast = new XBeeNode();
+            broadcast.Address16 = XBeeAddress16.ZNET_BROADCAST;
+            broadcast.Address64 = XBeeAddress64.BROADCAST;
+
+            TransmitDataRequest frame = new TransmitDataRequest(broadcast);
+            frame.FrameId = 1;
+            Assert.AreEqual(new byte[] { 0x10, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFE, 0x00, 0x00 }, frame.ToByteArray());
+        }
+
+        [Test]
+        public void TestTransmitDataRequestBroadcastRadius()
+        {
+            XBeeNode broadcast = new XBeeNode();
+            broadcast.Address16 = XBeeAddress16.ZNET_BROADCAST;
+            broadcast.Address64 = XBeeAddress64.BROADCAST;
+
+            TransmitDataRequest frame = new TransmitDataRequest(broadcast);
+            frame.BroadcastRadius = 2;
+            Assert.AreEqual(new byte[] { 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFE, 0x02, 0x00 }, frame.ToByteArray());
+        }
+
+        [Test]
+        public void TestTransmitDataRequestBroadcastRadiusOptions()
+        {
+            XBeeNode broadcast = new XBeeNode();
+            broadcast.Address16 = XBeeAddress16.ZNET_BROADCAST;
+            broadcast.Address64 = XBeeAddress64.BROADCAST;
+
+            TransmitDataRequest frame = new TransmitDataRequest(broadcast);
+            frame.BroadcastRadius = 2;
+            frame.Options = TransmitDataRequest.OptionValues.DISABLE_ACK | TransmitDataRequest.OptionValues.EXTENDED_TIMEOUT;
+            Assert.AreEqual(new byte[] { 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFE, 0x02, 0x41 }, frame.ToByteArray());
+        }
+
+        [Test]
+        public void TestTransmitDataRequestBroadcastRadiusOptionsData()
+        {
+            XBeeNode broadcast = new XBeeNode();
+            broadcast.Address16 = XBeeAddress16.ZNET_BROADCAST;
+            broadcast.Address64 = XBeeAddress64.BROADCAST;
+
+            TransmitDataRequest frame = new TransmitDataRequest(broadcast);
+            frame.BroadcastRadius = 2;
+            frame.Options = TransmitDataRequest.OptionValues.DISABLE_ACK | TransmitDataRequest.OptionValues.EXTENDED_TIMEOUT;
+            frame.SetRFData(new byte[] { 0x11, 0x22, 0x33, 0x00 });
+            Assert.AreEqual(new byte[] { 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFE, 0x02, 0x41, 0x11, 0x22, 0x33, 0x00 }, frame.ToByteArray());
+        }
+    }
+}
--- XBee/src/XBee/Frames/TransmitDataRequest.cs
+++ XBee/src/XBee/Frames/TransmitDataRequest.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using XBee;
 
 namespace XBee.Frames
 {
     public class TransmitDataRequest : XBeeFrame
     {
+        public enum OptionValues : byte
+        {
+            DISABLE_ACK = 0x01,
+            ENABLE_APS_ENCRYPTION = 0x20,
+            EXTENDED_TIMEOUT = 0x40
+        }
+
+        private XBeeNode destination;
+        private byte[] RFData;
+
+        public byte BroadcastRadius { get; set; }
+        public OptionValues Options { get; set; }
+
+        public TransmitDataRequest(XBeeNode destination)
+        {
+            this.commandId = XBeeAPICommandId.TRANSMIT_DATA_REQUEST;
+            this.destination = destination;
+            this.BroadcastRadius = 0;
+            this.Options = 0;
+            this.RFData = null;
+        }
+
+        public void SetRFData(byte[] RFData)
+        {
+            this.RFData = RFData;
+        }
 
         public override byte[] ToByteArray()
         {
-            return new byte[] { };
+            MemoryStream stream = new MemoryStream();
+
+            stream.WriteByte((byte)commandId);
+            stream.WriteByte(FrameId);
+
+            stream.Write(destination.Address64.GetAddress(), 0, 8);
+            stream.Write(destination.Address16.GetAddress(), 0, 2);
+
+            stream.WriteByte((byte)BroadcastRadius);
+            stream.WriteByte((byte)Options);
+
+            if (this.RFData != null) {
+                stream.Write(this.RFData, 0, this.RFData.Length);
+            }
+
+            return stream.ToArray();
         }
     }
 }
