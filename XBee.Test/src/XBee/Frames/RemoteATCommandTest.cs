7db5c9a86b3fe263797e90f72300a7ede0345b74
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-17 23:46:08 -0200

    Add Remote AT Command

---
+++ XBee.Test/src/XBee/Frames/RemoteATCommandTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using XBee;
+using XBee.Frames;
+
+namespace XBee.Test.Frames
+{
+    [TestFixture]
+    class RemoteATCommandTest
+    {
+        [Test]
+        public void TestRemoteATCommandND()
+        {
+            XBeeNode broadcast = new XBeeNode();
+            broadcast.Address16 = XBeeAddress16.ZNET_BROADCAST;
+            broadcast.Address64 = XBeeAddress64.BROADCAST;
+
+            RemoteATCommand cmd = new RemoteATCommand(AT.ND, broadcast);
+            cmd.FrameId = 1;
+
+            Assert.AreEqual(new byte[] { 0x17, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFE, 0x00, (byte)'N', (byte)'D' }, cmd.ToByteArray());
+        }
+
+        [Test]
+        public void TestRemoteATCommandDH()
+        {
+            XBeeNode broadcast = new XBeeNode();
+            broadcast.Address16 = XBeeAddress16.ZNET_BROADCAST;
+            broadcast.Address64 = XBeeAddress64.BROADCAST;
+
+            RemoteATCommand cmd = new RemoteATCommand(AT.DH, broadcast);
+            cmd.FrameId = 1;
+
+            Assert.AreEqual(new byte[] { 0x17, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFE, 0x00, (byte)'D', (byte)'H', 0x11, 0x22, 0x33, 0x00 }, cmd.ToByteArray());
+        }
+    }
+}
---
+++ XBee/src/XBee/Frames/RemoteATCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using XBee.Utils;
+
+namespace XBee.Frames
+{
+    public class RemoteATCommand : XBeeFrame
+    {
+        private AT atCommand;
+        private XBeeNode destination;
+
+        public byte RemoteOptions { get; set; }
+
+        public RemoteATCommand(AT atCommand, XBeeNode destination)
+        {
+            this.commandId = XBeeAPICommandId.REMOTE_AT_REQUEST;
+            this.atCommand = atCommand;
+            this.destination = destination;
+        }
+
+        public override byte[] ToByteArray()
+        {
+            MemoryStream stream = new MemoryStream();
+
+            stream.WriteByte((byte)commandId);
+            stream.WriteByte(FrameId);
+
+            stream.Write(destination.Address64.GetAddress(), 0, 8);
+            stream.Write(destination.Address16.GetAddress(), 0, 2);
+
+            stream.WriteByte(RemoteOptions);
+
+            var cmd = ((ATAttr)atCommand.GetAttr()).ATCommand.ToCharArray();
+            stream.WriteByte((byte)cmd[0]);
+            stream.WriteByte((byte)cmd[1]);
+
+            return stream.ToArray();
+        }
+    }
+}
