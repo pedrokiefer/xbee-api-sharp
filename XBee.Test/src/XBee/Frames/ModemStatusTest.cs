1684dd1f7aa5c015126ee2e88782ab202be00310
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-19 14:19:27 -0200

    Add test stubs

---
+++ XBee.Test/src/XBee/Frames/ModemStatusTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NUnit.Framework;
+using XBee;
+using XBee.Frames;
+
+namespace XBee.Test.Frames
+{
+    [TestFixture]
+    public class ModemStatusTest
+    {
+
+    }
+}
