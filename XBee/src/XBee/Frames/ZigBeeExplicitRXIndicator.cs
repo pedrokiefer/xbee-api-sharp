c3540171f6bb3d79dbcf6dd6e49d7441820e4468
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-21 18:24:28 -0200

    Add ExplicitRXIndicator parsing support

--- XBee/src/XBee/Frames/ZigBeeExplicitRXIndicator.cs
+++ XBee/src/XBee/Frames/ZigBeeExplicitRXIndicator.cs
@@ -7,8 +7,20 @@ namespace XBee.Frames
 {
     public class ZigBeeExplicitRXIndicator : XBeeFrame
     {
-        public ZigBeeExplicitRXIndicator()
+        private readonly PacketParser parser;
+
+        public XBeeNode Source { get; private set; }
+        public byte? SourceEndpoint { get; private set; }
+        public byte? DestinationEndpoint { get; private set; }
+        public UInt16? ClusterId { get; private set; }
+        public UInt16? ProfileId { get; private set; }
+
+        public ReceiveOptionsType ReceiveOptions { get; private set; }
+        public byte[] Data { get; private set; }
+
+        public ZigBeeExplicitRXIndicator(PacketParser parser)
         {
+            this.parser = parser;
             this.CommandId = XBeeAPICommandId.EXPLICIT_RX_INDICATOR_RESPONSE;
         }
 
@@ -19,7 +31,15 @@ namespace XBee.Frames
 
         public override void Parse()
         {
-            throw new NotImplementedException();
+            Source = new XBeeNode { Address64 = parser.ReadAddress64(), Address16 = parser.ReadAddress16() };
+
+            SourceEndpoint = (byte?) parser.ReadByte();
+            DestinationEndpoint = (byte?) parser.ReadByte();
+            ClusterId = (UInt16?) parser.ReadUInt16();
+            ProfileId = (UInt16?) parser.ReadUInt16();
+
+            ReceiveOptions = (ReceiveOptionsType) parser.ReadByte();
+            Data = parser.ReadData();
         }
     }
 
