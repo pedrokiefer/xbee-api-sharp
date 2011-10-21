349df56ab0b9aa7d83df33b674f9737307d906dc
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-21 18:24:59 -0200

    Add ZigBeeReceivePacket parsing support

--- XBee/src/XBee/Frames/ZigBeeReceivePacket.cs
+++ XBee/src/XBee/Frames/ZigBeeReceivePacket.cs
@@ -7,8 +7,15 @@ namespace XBee.Frames
 {
     public class ZigBeeReceivePacket : XBeeFrame
     {
-        public ZigBeeReceivePacket()
+        private readonly PacketParser parser;
+
+        public XBeeNode Source { get; private set; }
+        public ReceiveOptionsType ReceiveOptions { get; private set; }
+        public byte[] Data { get; private set; }
+
+        public ZigBeeReceivePacket(PacketParser parser)
         {
+            this.parser = parser;
             this.CommandId = XBeeAPICommandId.RECEIVE_PACKET_RESPONSE;
         }
 
@@ -19,7 +26,9 @@ namespace XBee.Frames
 
         public override void Parse()
         {
-            throw new NotImplementedException();
+            Source = new XBeeNode { Address64 = parser.ReadAddress64(), Address16 = parser.ReadAddress16() };
+            ReceiveOptions = (ReceiveOptionsType) parser.ReadByte();
+            Data = parser.ReadData();
         }
     }
 }
