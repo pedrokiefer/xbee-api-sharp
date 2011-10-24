c8008350c4bd2cd0a228574a68bb67be69d412ac
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-24 18:30:19 -0200

    Add sample application

---
+++ XBee.Sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XBee;
+using XBee.Frames;
+
+namespace XBee.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bee = new XBee();
+
+            bee.SetConnection(new SerialConnection("COM4", 9600));
+            var request = new ATCommand(AT.AP) {FrameId = 1};
+
+            var frame = bee.SendSynchronous(request, 1000);
+
+        }
+    }
+}
