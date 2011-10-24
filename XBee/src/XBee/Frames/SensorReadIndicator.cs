1844c6d7c322a6f4e50d94623101bda9bb9efa72
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-24 13:07:20 -0200

    Add Sensor Read Indicator frame support

--- XBee/src/XBee/Frames/SensorReadIndicator.cs
+++ XBee/src/XBee/Frames/SensorReadIndicator.cs
@@ -7,9 +7,27 @@ namespace XBee.Frames
 {
     public class SensorReadIndicator : XBeeFrame
     {
-        public SensorReadIndicator()
+        [Flags]
+        public enum SensorType
         {
-            this.CommandId = XBeeAPICommandId.SENSOR_READ_INDICATOR;
+            ADSensor = 0x01,
+            TemperatureSensor = 0x02,
+            WaterPresent = 0x60
+        }
+
+        private readonly PacketParser parser;
+
+        public XBeeNode Source { get; private set; }
+        public ReceiveOptionsType ReceiveOptions { get; private set; }
+        public SensorType Sensors { get; private set; }
+        public ushort[] ADValues { get; private set; }
+        public ushort Temperature { get; private set; }
+
+        public SensorReadIndicator(PacketParser parser)
+        {
+            this.parser = parser;
+            CommandId = XBeeAPICommandId.SENSOR_READ_INDICATOR;
+            ADValues = new ushort[4] { 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF };
         }
 
         public override byte[] ToByteArray()
@@ -19,7 +37,20 @@ namespace XBee.Frames
 
         public override void Parse()
         {
-            throw new NotImplementedException();
+            Source = new XBeeNode { Address64 = parser.ReadAddress64(), Address16 = parser.ReadAddress16() };
+            ReceiveOptions = (ReceiveOptionsType) parser.ReadByte();
+            Sensors = (SensorType) parser.ReadByte();
+            for (var i = 0; i < 4; i++) {
+                ADValues[i] = parser.ReadUInt16();
+            }
+            Temperature = parser.ReadUInt16();
+        }
+
+        public double GetCelciusTemperature()
+        {
+            if (Temperature < 0x800)
+                return Temperature / 16.0;
+            return -(Temperature & 0x7FF) / 16.0;
         }
     }
 }
