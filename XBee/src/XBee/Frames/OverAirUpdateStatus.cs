b0bac5521c7a059f6d71e8020946aea04d2bf39c
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-23 23:28:28 -0200

    Implement OverAirUpdateStatus frame

--- XBee/src/XBee/Frames/OverAirUpdateStatus.cs
+++ XBee/src/XBee/Frames/OverAirUpdateStatus.cs
@@ -5,11 +5,30 @@ using System.Text;
 
 namespace XBee.Frames
 {
+
     public class OverAirUpdateStatus : XBeeFrame
     {
-        public OverAirUpdateStatus()
+        public enum BootloaderMessageType
         {
-            this.CommandId = XBeeAPICommandId.FIRMWARE_UPDATE_STATUS;
+            Ack = 0x06,
+            NAck = 0x15,
+            NoMacAck = 0x40,
+            Query = 0x51,
+            QueryResponse = 0x52
+        }
+
+        private readonly PacketParser parser;
+
+        public XBeeNode Source { get; private set; }
+        public ReceiveOptionsType ReceiveOptions { get; private set; }
+        public BootloaderMessageType BootloaderMessage { get; private set; }
+        public int BlockNumber { get; private set; }
+        public XBeeAddress64 Target { get; private set; }
+
+        public OverAirUpdateStatus(PacketParser parser)
+        {
+            this.parser = parser;
+            CommandId = XBeeAPICommandId.FIRMWARE_UPDATE_STATUS;
         }
 
         public override byte[] ToByteArray()
@@ -19,7 +38,11 @@ namespace XBee.Frames
 
         public override void Parse()
         {
-            throw new NotImplementedException();
+            Source = new XBeeNode { Address64 = parser.ReadAddress64(), Address16 = parser.ReadAddress16() };
+            ReceiveOptions = (ReceiveOptionsType) parser.ReadByte();
+            BootloaderMessage = (BootloaderMessageType) parser.ReadByte();
+            BlockNumber = parser.ReadByte();
+            Target = parser.ReadAddress64();
         }
     }
 }
