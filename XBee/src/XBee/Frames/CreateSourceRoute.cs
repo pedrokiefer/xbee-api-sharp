e14e1bae3a17eefffeaa3ef27a8f17258bfff384
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-24 13:33:12 -0200

    Add Create Source Route support

--- XBee/src/XBee/Frames/CreateSourceRoute.cs
+++ XBee/src/XBee/Frames/CreateSourceRoute.cs
@@ -1,25 +1,54 @@
-﻿namespace XBee.Frames
+﻿using System.Collections.Generic;
+using System.IO;
+using XBee.Exceptions;
+
+namespace XBee.Frames
 {
     public class CreateSourceRoute : XBeeFrame
     {
-
-        private XBeeNode destination;
         private readonly PacketParser parser;
+        private XBeeNode destination;
+
+        public byte RouteOptions { get; set; }
+        public List<XBeeAddress16> Hops { get; set; }
 
         public CreateSourceRoute(PacketParser parser)
         {
             this.parser = parser;
-            this.CommandId = XBeeAPICommandId.CREATE_SOURCE_ROUTE;
+            CommandId = XBeeAPICommandId.CREATE_SOURCE_ROUTE;
+            RouteOptions = 0x00;
+            FrameId = 0x00;
         }
 
-        public CreateSourceRoute()
+        public CreateSourceRoute(XBeeNode destination)
         {
-            this.CommandId = XBeeAPICommandId.CREATE_SOURCE_ROUTE;
+            CommandId = XBeeAPICommandId.CREATE_SOURCE_ROUTE;
+            this.destination = destination;
+            RouteOptions = 0x00;
+            FrameId = 0x00;
         }
 
         public override byte[] ToByteArray()
         {
-            return new byte[] { };
+            if (Hops.Count == 0)
+                throw new XBeeFrameException("Missing Hops List");
+
+            var frame = new MemoryStream();
+
+            frame.WriteByte((byte) CommandId);
+            frame.WriteByte(FrameId);
+
+            frame.Write(destination.Address64.GetAddress(), 0, 8);
+            frame.Write(destination.Address16.GetAddress(), 0, 2);
+
+            frame.WriteByte(RouteOptions);
+            frame.WriteByte((byte)Hops.Count);
+
+            foreach(var addr in Hops) {
+                frame.Write(addr.GetAddress(), 0, 2);
+            }
+
+            return frame.ToArray();
         }
 
         public override void Parse()
