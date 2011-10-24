3b574fb782a99ed5153f19d8a1c2864b285abcba
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-24 00:22:04 -0200

    Use extended enum attributes

--- XBee/src/XBee/XBeeHardwareVersion.cs
+++ XBee/src/XBee/XBeeHardwareVersion.cs
@@ -1,69 +1,31 @@
-﻿using System;
-using NLog;
+﻿using XBee.Utils;
 
 namespace XBee
 {
-    using System;
-    using NLog;
+
+    public class XBeeHardwareAttribute : EnumAttribute
+    {
+        public string Name { get; private set; }
+
+        public XBeeHardwareAttribute(string name)
+        {
+            Name = name;
+        }
+    }
 
     public enum XBeeSeriesId : byte
     {
+        [XBeeHardware("XBee Series 1")]
         XBEE_SERIES_1 = 0x17,
+        [XBeeHardware("XBee Series 1 Pro")]
         XBEE_SERIES_1_PRO = 0x18,
+        [XBeeHardware("XBee Series 2")]
         XBEE_SERIES_2 = 0x19,
+        [XBeeHardware("XBee Series 2 Pro")]
         XBEE_SERIES_2_PRO = 0x1A,
+        [XBeeHardware("XBee Series 2B Pro")]
         XBEE_SERIES_2B_PRO = 0x1E,
+        [XBeeHardware("XBee Unknown")]
         XBEE_SERIES_UNKNOWN = 0xFF
     };
-
-    public class XbeeHardwareVersion
-    {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
-
-        private static String xbeeSeries1 = "XBee Series 1";
-        private static String xbeeSeries1Pro = "XBee Series 1 Pro";
-        private static String xbeeSeries2 = "XBee Series 2";
-        private static String xbeeSeries2Pro = "XBee Series 2 Pro";
-        private static String xbeeSeries2BPro = "XBee Series 2B Pro";
-        private static String xbeeUnknown = "XBee Unknown";
-
-        public static XBeeSeriesId GetHardwareVersion(byte version) 
-        {
-            switch ((XBeeSeriesId)version)
-            {
-                case XBeeSeriesId.XBEE_SERIES_1:
-                    return XBeeSeriesId.XBEE_SERIES_1;
-                case XBeeSeriesId.XBEE_SERIES_1_PRO:
-                    return XBeeSeriesId.XBEE_SERIES_1_PRO;
-                case XBeeSeriesId.XBEE_SERIES_2:
-                    return XBeeSeriesId.XBEE_SERIES_2;
-                case XBeeSeriesId.XBEE_SERIES_2_PRO:
-                    return XBeeSeriesId.XBEE_SERIES_2_PRO;
-                case XBeeSeriesId.XBEE_SERIES_2B_PRO:
-                    return XBeeSeriesId.XBEE_SERIES_2B_PRO;
-                default:
-                    return XBeeSeriesId.XBEE_SERIES_UNKNOWN;
-            }
-        }
-
-        public static String GetHardwareVersionDescription(XBeeSeriesId version)
-        {
-            switch ((XBeeSeriesId)version)
-            {
-                case XBeeSeriesId.XBEE_SERIES_1:
-                    return xbeeSeries1;
-                case XBeeSeriesId.XBEE_SERIES_1_PRO:
-                    return xbeeSeries1Pro;
-                case XBeeSeriesId.XBEE_SERIES_2:
-                    return xbeeSeries2;
-                case XBeeSeriesId.XBEE_SERIES_2_PRO:
-                    return xbeeSeries2Pro;
-                case XBeeSeriesId.XBEE_SERIES_2B_PRO:
-                    return xbeeSeries2BPro;
-                default:
-                    return xbeeUnknown;
-            }
-        }
-
-    }
 }
