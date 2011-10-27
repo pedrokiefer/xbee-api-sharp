0683bd927370be9d151495a5f4afc06f8d1b6d20
Pedro Kiefer <pedro@kiefer.com.br>
2011-10-27 16:30:41 -0200

    Query for more AT values

--- XBee.Sample/Program.cs
+++ XBee.Sample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using XBee;
 using XBee.Frames;
 
@@ -12,12 +13,23 @@ namespace XBee.Sample
         static void Main(string[] args)
         {
             var bee = new XBee();
-
+            bee.ApiType = ApiTypeValue.EnabledWithEscape;
             bee.SetConnection(new SerialConnection("COM4", 9600));
+
             var request = new ATCommand(AT.ApiEnable) {FrameId = 1};
-
             var frame = bee.SendSynchronous(request, 1000);
+            var value = ((ATCommandResponse) frame).Value;
+            Console.WriteLine(String.Format("API type: {0}", ((ATLongValue)value).Value));
 
+            request = new ATCommand(AT.BaudRate) { FrameId = 1 };
+            frame = bee.SendSynchronous(request, 1000);
+            value = ((ATCommandResponse) frame).Value;
+            Console.WriteLine(String.Format("Baud rate: {0}", ((ATLongValue) value).Value));
+
+            request = new ATCommand(AT.FirmwareVersion) { FrameId = 1 };
+            frame = bee.SendSynchronous(request, 1000);
+            value = ((ATCommandResponse) frame).Value;
+            Console.WriteLine(String.Format("Firmware Version: {0:X4}", ((ATLongValue) value).Value));
         }
     }
 }
